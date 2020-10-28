44c8d8ed778ec6482b1a39f5fb28243d131de39e
MauritsvanderZee <maurits@van-der-zee.de>
2020-10-28 10:27:01 +0100

    moved header and footer to separate files; replaced code with include statements;

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Trale organization</h5>
+        <p class="grey-text text-lighten-4">High quality privacy solutions for individuals and teams.</p>
+      </div>
+      <div class="col l2 offset-l2 s6">
+        <h6>About</h6>
+        <ul>
+          <li><a href="" class="grey-text text-lighten-3">Help</a></li>
+          <li><a href="" class="grey-text text-lighten-3">Contact</a></li>
+          <li><a href="" class="grey-text text-lighten-3">Suggestions</a></li>
+        </ul>
+      </div>
+      <div class="col l2 s6">
+        <h6>Developer</h6>
+        <ul>
+          <li><a href="" class="grey-text text-lighten-3">Source</a></li>
+          <li><a href="" class="grey-text text-lighten-3">Contribute</a></li>
+          <li><a href="" class="grey-text text-lighten-3">API Docs</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="center">
+        <span>&copy;2020 Trale</span>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+  <nav>
+    <section class="container">
+      <div class="nav-wrapper">
+        <section id="brand-logo">
+          <a href="/">
+            <img src="https://cdn.code-lake.com/socialstuff/img/eye.svg" alt="CDN unavailable" id="logo">
+          </a>
+        </section>
+        <a href="/" id="brand-name">Trale</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="/">Home</a></li>
+          <li><a href="/features">Features</a></li>
+          <li><a href="/team">Team</a></li>
+          <li><a href="/faq">FAQ</a></li>
+        </ul>
+      </div>
+    </section>
+  </nav>
+</header>
