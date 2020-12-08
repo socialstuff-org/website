6e2207170bf6120f88459114c00b094e2c35d6ea
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-08 17:22:08 +0100

    updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,24 +1,44 @@
 <footer class="page-footer">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
+      <div class="col l4 s12">
         <h5 class="white-text">Trale organization</h5>
-        <p class="grey-text text-lighten-4">High quality privacy solutions for individuals and teams.</p>
+        <p class="grey-text text-lighten-4">
+          High quality privacy solutions for individuals and teams.<br>
+        </p>
       </div>
-      <div class="col l2 offset-l2 s6">
-        <h6>About</h6>
+      <div class="col l2 s6">
+        <h6>Company</h6>
         <ul>
-          <li><a href="" class="grey-text text-lighten-3">Help</a></li>
-          <li><a href="" class="grey-text text-lighten-3">Contact</a></li>
-          <li><a href="" class="grey-text text-lighten-3">Suggestions</a></li>
+          <li><a href="https://trale.org/donate" class="grey-text text-lighten-3">Donate</a></li>
+          <li><a href="https://careers.trale.org" class="grey-text text-lighten-3">Careers</a></li>
+          <li><a href="https://blog.trale.org" class="grey-text text-lighten-3">Blog</a></li>
+          <li><a href="https://trale.org/terms" class="grey-text text-lighten-3">Terms & Privacy Policy</a></li>
         </ul>
       </div>
       <div class="col l2 s6">
-        <h6>Developer</h6>
+        <h6>Download</h6>
         <ul>
-          <li><a href="" class="grey-text text-lighten-3">Source</a></li>
-          <li><a href="" class="grey-text text-lighten-3">Contribute</a></li>
-          <li><a href="" class="grey-text text-lighten-3">API Docs</a></li>
+          <li><a href="https://downloads.trale.org#android" class="grey-text text-lighten-3">Android</a></li>
+          <li><a href="https://downloads.trale.org#ios" class="grey-text text-lighten-3">iPhone & iPad</a></li>
+          <li><a href="https://downloads.trale.org#windows" class="grey-text text-lighten-3">Windows</a></li>
+          <li><a href="https://downloads.trale.org#macos" class="grey-text text-lighten-3">Mac</a></li>
+          <li><a href="https://downloads.trale.org#linux" class="grey-text text-lighten-3">Linux</a></li>
+        </ul>
+      </div>
+      <div class="col l2 s6">
+        <h6>Social</h6>
+        <ul>
+          <li><a href="https://github.com/socialstuff-org" class="grey-text text-lighten-3">GitHub</a></li>
+          <li><a href="https://twitter.com" class="grey-text text-lighten-3">Twitter</a></li>
+          <li><a href="https://instagram.com" class="grey-text text-lighten-3">Instagram</a></li>
+        </ul>
+      </div>
+      <div class="col l2 s6">
+        <h6>Help</h6>
+        <ul>
+          <li><a href="https://support.trale.org" class="grey-text text-lighten-3">Support</a></li>
+          <li><a href="https://docs.trale.org" class="grey-text text-lighten-3">Documentation</a></li>
         </ul>
       </div>
     </div>
@@ -26,7 +46,7 @@
   <div class="footer-copyright">
     <div class="container">
       <div class="center">
-        <span>&copy;2020 Trale</span>
+        <span>&copy;2020 Trale</span><br>
       </div>
     </div>
   </div>
