105fb09c6762af4d21cdad2d4259ffbd276a4922
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-08 19:53:52 +0100

    implemented donate view

---
+++ resources/views/donate.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title', 'Donate')
+
+@section('content')
+
+
+  <section class="bg-elderberry">
+
+    <section class="row"><!-- SPACER --></section>
+
+    <div class="row center clear-margin">
+      <div class="col s12 m6 offset-m3 l4 offset-l4">
+        <section class="col s12 m12 l10 offset-l1 xl8 offset-xl2 left-align">
+          <h2>Donate to Trale</h2>
+          <p>
+            The team at Trale is committed to the mission of developing open source privacy technology that protects
+            free
+            expression and enables secure global communication. Your contribution fuels this cause. No ads. No trackers.
+            No
+            compromise.
+          </p>
+          <p>
+            Your donation helps pay for the development of an app used by millions around the world for private and
+            instantaneous communication.
+          </p>
+          <p>
+            <span style="font-weight: bold;">Please make a donation today.</span>
+          </p>
+          <p>
+            Double the impact of your donation. Ask your employer if they offer a company match.
+          </p>
+          <section class="center" style="background-color: #C72C41; border-radius: 3px; width: auto; padding: 10px;">
+            Donate via PayPal to:<br>
+            <span style="font-weight: bold;">donate@trale.org</span>
+          </section>
+        </section>
+      </div>
+    </div>
+
+    <section class="row clear-margin bg-elderberry" style="margin-top: 40px;"><!-- SPACER --></section>
+
+  </section>
+
+@endsection
