a8314e73d7069ca9ecbfcaf82969f6201e3ed609
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-08 20:30:00 +0100

    added careers view

---
+++ resources/views/careers.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('title', 'Careers')
+
+@section('content')
+
+
+  <section class="bg-elderberry">
+
+    <section class="row"><!-- SPACER --></section>
+
+    <div class="row center clear-margin">
+      <div class="col s12 m6 offset-m3 l4 offset-l4">
+        <section class="col s12 m12 l10 offset-l1 xl8 offset-xl2 left-align">
+          <h2>Careers</h2>
+          <p>
+            Currently we are not hiring new people. Please make sure to check again from time to time.
+          </p>
+        </section>
+      </div>
+    </div>
+
+    <section class="row clear-margin bg-elderberry" style="margin-top: 40px;"><!-- SPACER --></section>
+
+  </section>
+
+@endsection
